a8554482304ce75d72f909b543a5219e10e4e8d6
Vladislav Savchenko <vs101ff@gmail.com>
2021-04-12 16:22:59 +0300

    chore: add start workspace script

--- scripts/workspace/start.sh
+++ scripts/workspace/start.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 echo "┏━━━ 🚀 STARTING SERVER ━━━━━━━━━━━━━━━━━━━━━━━━━━"
-# lerna run dev --scope @lucid/app1 --stream
-node packages/app1/server/server.js
+lerna run start --scope @lucid/app1 --stream
