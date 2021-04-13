e07dd5363069fdf37270bd265e72949f29c37c6c
Vladislav Savchenko <vs101ff@gmail.com>
2021-04-13 11:58:04 +0300

    fix(util-data-access): add api client

---
+++ packages/util-data-access/src/api-client.ts
@@ -0,0 +1,73 @@
+// eslint-disable-next-line @typescript-eslint/ban-types
+const isPlainObject = (value: unknown): value is object => {
+  return (
+    typeof value === 'object' &&
+    !Array.isArray(value) &&
+    value !== null &&
+    !(value instanceof Function) &&
+    !(value instanceof Date)
+  );
+};
+
+async function client(
+  endpoint: string,
+  {
+    data,
+    token,
+    ...customConfig
+  }: RequestInit & {
+    data?: unknown;
+    token?: string;
+  } = {},
+): Promise<any> {
+  let config: RequestInit = {
+    headers: {},
+  };
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  function addDataToUrlSearchParams(data: object): string {
+    const url = new URL(endpoint);
+
+    for (const [name, value] of Object.entries(data)) {
+      url.searchParams.append(
+        name,
+        typeof value === 'string' ? value : JSON.stringify(value),
+      );
+    }
+
+    return url.toString();
+  }
+
+  if (data) {
+    if (
+      customConfig.method != null &&
+      ['GET', 'HEAD'].includes(customConfig.method)
+    ) {
+      if (isPlainObject(data)) {
+        endpoint = addDataToUrlSearchParams(data);
+      }
+    } else {
+      config.body = JSON.stringify(data);
+      (config.headers as any)['Content-Type'] = 'application/json';
+    }
+  } else if (!customConfig.method) {
+    config.method = 'GET';
+  }
+
+  if (token) {
+    (config.headers as any).Authorization = `Bearer ${token}`;
+  }
+
+  config = {
+    ...config,
+    ...customConfig,
+    headers: {
+      ...config.headers,
+      ...customConfig.headers,
+    },
+  };
+
+  return window.fetch(endpoint, config);
+}
+
+export { client };
