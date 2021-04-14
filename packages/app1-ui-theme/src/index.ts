7fdc4884d804080d63bd07fa4a5f9b0851806db6
Vladislav Savchenko <vs101ff@gmail.com>
2021-04-14 17:29:27 +0300

    feat(app1-ui-theme): add global styles

--- packages/app1-ui-theme/src/index.ts
+++ packages/app1-ui-theme/src/index.ts
@@ -1,9 +1,11 @@
 import foundations from './foundations';
+import styles from './styles';
 
 // Highly inspired by awesome library https://github.com/chakra-ui/chakra-ui
 
 export const theme = {
   ...foundations,
+  styles,
 };
 
 export type Theme = typeof theme;
