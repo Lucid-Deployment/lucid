7ee77132a92a3c20d82a1e0afda499406242e8dd
Vladislav Savchenko <vs101ff@gmail.com>
2021-04-14 10:52:43 +0300

    feat(catalog-data-access): add isVisible, price, dateModified, availability to Product api interface and get all products request type

--- packages/catalog/api-interfaces/src/products.ts
+++ packages/catalog/api-interfaces/src/products.ts
@@ -1,14 +1,25 @@
 import { IGetAllConfig } from './common';
 
+type ProductAvailability = 'available' | 'disabled' | 'preorder';
 interface Product {
   id: string;
   name: string;
   sku: string;
+  isVisible?: boolean;
+  price?: number;
+  dateModified: string;
+  availability?: ProductAvailability;
 }
 
 type GetAllProductsData = Product[];
 
-type SortableProductFields = 'id' | 'name' | 'sku';
+type SortableProductFields =
+  | 'id'
+  | 'name'
+  | 'sku'
+  | 'price'
+  | 'dateModified'
+  | 'isVisible';
 interface GetAllProductsRequest extends IGetAllConfig, Partial<Product> {
   idIn?: Product['id'][];
   idNotIn?: Product['id'][];
