b2ee5ae8b5c903ba14bc2b9c4a482f944d2cfd02
Vladislav Savchenko <vs101ff@gmail.com>
2021-04-22 12:40:38 +0300

    test(app1-ui-shared): add Pricing story

---
+++ packages/app1-ui-shared/stories/marketing/Pricing.stories.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+import { Pricing as PricingComponent } from "../../src"
+import type { Meta } from "@storybook/react"
+
+export default {
+  title: "Organisms/Marketing/Pricing",
+  component: PricingComponent,
+  decorators: [],
+} as Meta
+
+export const Pricing = () => <PricingComponent />
