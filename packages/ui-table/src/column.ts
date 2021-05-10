e9c7c38c6454c396d3b35ebf140f33bde1a79c30
Vladislav Savchenko <vs101ff@gmail.com>
2021-05-10 09:51:12 +0300

    feat(ui-table): add simple table component

---
+++ packages/ui-table/src/column.ts
@@ -0,0 +1,15 @@
+import type { ComponentProps } from "@lucid/feature-component"
+
+export type ColumnType = "default" | "boolean" | "button"
+
+export const columnTypeTitles: Record<ColumnType, string> = {
+  default: "Default",
+  boolean: "Boolean",
+  button: "Button",
+}
+
+export interface Column extends ComponentProps {
+  title: string
+  columnType: ColumnType
+  editable: boolean
+}
---
+++ packages/ui-table/src/table.tsx
@@ -0,0 +1,47 @@
+import type { ComponentProps } from "@lucid/feature-component"
+import {
+  Grid,
+  GridItem,
+  Box,
+  useColorModeValue,
+  GridItemProps,
+  Flex,
+} from "@chakra-ui/react"
+import * as React from "react"
+import type { Column } from "./column"
+
+export function TableCell(props: GridItemProps) {
+  return (
+    <GridItem
+      borderW="1px"
+      borderColor={useColorModeValue("gray.400", "whiteAlpha.500")}
+      {...props}
+    />
+  )
+}
+
+export interface TableProps extends ComponentProps {
+  columns: Column[]
+  data: any
+}
+export function Table({ columns, data }: TableProps) {
+  return (
+    <Box>
+      <Grid templateColumns={`repeat(${columns.length}, 1fr)`}>
+        {data.map((x: any, i: number) =>
+          columns.map((c) => (
+            <TableCell key={`${i}_${c.id}`}>${x[i][c.id]}</TableCell>
+          )),
+        )}
+      </Grid>
+      <Flex
+        bg={useColorModeValue("gray.300", "whiteAlpha.400")}
+        justify="space-between"
+        items="center"
+        wrap="wrap"
+      >
+        <Box>{data.length} results</Box>
+      </Flex>
+    </Box>
+  )
+}
