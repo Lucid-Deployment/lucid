904e980e3889165f91171e4e361570e2aac9b777
Vladislav Savchenko <vs101ff@gmail.com>
2021-05-10 10:15:38 +0300

    fix(ui-dashboard-shell): fix typo in stories name

--- packages/ui-dashboard-shell/stories/DashboardShell.stories.tsx
+++ packages/ui-dashboard-shell/stories/DashboardShell.stories.tsx
@@ -3,7 +3,7 @@ import { DashboardShell as DashboardShellComponent } from "../src"
 import type { Meta } from "@storybook/react"
 
 export default {
-  title: "Pages/DashboardShell",
+  title: "Pages/Dashboard Shell",
   component: DashboardShellComponent,
   decorators: [],
 } as Meta
