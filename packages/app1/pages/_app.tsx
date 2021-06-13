fa6dee432b078f152fe66ab263fc6230e2184cf1
Vladislav Savchenko <vs101ff@gmail.com>
2021-06-13 09:23:21 +0300

    feat(app1): enclose app in stacking context with 0 z-index

--- packages/app1/pages/_app.tsx
+++ packages/app1/pages/_app.tsx
@@ -1,12 +1,14 @@
 import type { AppProps } from "next/app"
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider, chakra } from "@chakra-ui/react"
 import theme from "@lucid/app1-ui-theme"
 import * as React from "react"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <chakra.div position="relative" zIndex={0}>
+        <Component {...pageProps} />
+      </chakra.div>
     </ChakraProvider>
   )
 }
