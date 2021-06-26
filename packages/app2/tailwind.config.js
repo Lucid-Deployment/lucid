758f482098666a80c2f77213176a0f8de60c12eb
Vladislav Savchenko <vs101ff@gmail.com>
2021-06-26 10:55:00 +0300

    feat(app2): add top nav

--- packages/app2/tailwind.config.js
+++ packages/app2/tailwind.config.js
@@ -1,3 +1,11 @@
+const blue = {
+  600: "#0263e0",
+}
+
+const gray = {
+  200: "#e1e3ea",
+}
+
 module.exports = {
   mode: "jit",
   purge: [
@@ -5,9 +13,33 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
     "./features/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: "media", // or 'media' or 'class'
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue,
+        brand: blue,
+        gray,
+      },
+      textColor: {
+        primary: "#1f304c",
+      },
+      transitionDuration: {
+        250: "250ms",
+      },
+      zIndex: {
+        5: "5",
+      },
+      transitionTimingFunction: {
+        "xs-slow-acceleration": "cubic-bezier(0.4, 0, 1, 1)",
+        "xs-slow-acceleration-md-slower-deceleration":
+          "cubic-bezier(0.4, 0, 0.2, 1)",
+      },
+      boxShadow: {
+        xl: "0 8px 32px rgb(18 28 45 / 10%)",
+        "gray-200-hr-1": `0 1px 0 0 ${gray["200"]}`,
+      },
+    },
   },
   variants: {
     extend: {},
