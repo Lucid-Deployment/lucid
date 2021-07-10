bb525b28d547c480617a0a4ddedad30d8fa2b0fe
Vladislav Savchenko <vs101ff@gmail.com>
2021-07-10 06:01:16 +0300

    fix(app1): remove endpoints / pages

--- packages/app1/pages/api/zz/[pid].ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { field1 } = req.body;
-  const { pid } = req.query;
-  res.json({ field1, pid });
-};
