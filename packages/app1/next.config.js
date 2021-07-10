9c2fc8680677202371134231436d223146ed38e6
Vladislav Savchenko <vs101ff@gmail.com>
2021-07-10 06:43:21 +0300

    fix(app1): env

--- packages/app1/next.config.js
+++ packages/app1/next.config.js
@@ -5,8 +5,5 @@ module.exports = {
       sizeLimit: "500kb",
     },
   },
-  env: {
-    ATLAS_URL: "mongodb://app1:secret123@localhost:27017/app1?authSource=admin",
-    NEXT_API_URL: "http://localhost:3000/api",
-  },
+  env: {},
 };
