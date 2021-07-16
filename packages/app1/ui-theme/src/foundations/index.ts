d1c4b984d64f75d6ba659efeb8e4af59c8845b58
Vladislav Savchenko <vs101ff@gmail.com>
2021-07-16 11:49:20 +0300

    fix(app1-ui-theme): remove theme parts duplicating defaults

--- packages/app1/ui-theme/src/foundations/index.ts
+++ packages/app1/ui-theme/src/foundations/index.ts
@@ -1,20 +1,12 @@
-import breakpoints from "./breakpoints";
 import colors from "./colors";
-import shadows from "./shadows";
-import sizes from "./sizes";
-import { spacing } from "./spacing";
 import transition from "./transition";
 import typography from "./typography";
 import zIndices from "./z-index";
 
 const foundations = {
-  breakpoints,
   zIndices,
   colors,
   ...typography,
-  sizes,
-  shadows,
-  space: spacing,
   transition,
 };
 
